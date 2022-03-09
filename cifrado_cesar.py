b5363d302d97d046273649a513a086a9182bd2b8
Denis Radu <mariodenisradu1705@gmail.com>
2022-03-09 23:09:08 +0100

    commit inicial

---
+++ cifrado_cesar.py
@@ -0,0 +1,16 @@
+
+def cifradoCesar():
+    a = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+    b = str(input("dime la palabra a encriptar: "))
+    c = int(input("dime la clave: "))
+    mensaje = ""
+    for i in b.upper():
+        if i in a:
+            indice = a.find(i)
+            indice += c
+            if indice >= 27:
+                indice -= 27
+            mensaje += a[indice]
+        else:
+            mensaje += i
+    print(mensaje)
